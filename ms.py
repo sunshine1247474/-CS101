04e5ae3172ac4fc5d9c7c2cdd1d2912702878270
Itay <itay.shklyar@gmail.com>
2022-04-03 12:55:57 +0300

    1

--- ms.py
+++ ms.py
@@ -1,3 +1,1 @@
-print("trying saving on github through pycharm for the first time")
-print("adding second line for testing")
-print("making it available at the current branch")
+print("this commit will always stay here")
